9cb75f7789c0ae6d07da81749d750deb0489b76a
Artur Araripe <arturararipe@gmail.com>
2022-10-11 04:25:23 -0300

    Update bot1.js

--- bot1.js
+++ bot1.js
@@ -12,7 +12,7 @@ const bot = new Fosscord.Client({
 
 bot.on("message", message => {
     if (message.author.bot) return;
-    if (message.content.indexOf(config.prefix) !== 0) return;
+    if (message.content.indexOf(prefix) !== 0) return;
 
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase()
