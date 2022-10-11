fad67aeb76f090a86bba67713625baa42e90d8ac
Artur Araripe <arturararipe@gmail.com>
2022-10-11 16:24:10 -0300

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,7 +1,7 @@
 import Fosscord from "fosscord-gopnik";
 const prefix = '!' 
 const bot = new Fosscord.Client({
-        intents: ["GUILD_MESSAGES"],
+        intents: ["GUILD_MESSAGES,GUILD_MEMBER_ADD,MESSAGE_CREATE"],
         // New default endpoints:
         http: {
                 api: 'https://gadajcord.us/api',
@@ -18,24 +18,24 @@ bot.on('ready', () => console.log('The Bot is ready!'));
 
 //Welcome & goodbye messages\\
 bot.on('guildMemberAdd', member => {
-    member.roles.add(member.guild.roles.cache.find(i => i.name === 'Gadajcord'))
+    member.roles.add(member.guild.roles.cache.find(i => i.name === 'Member'))
 
-    const welcomeEmbed = new Discord.MessageEmbed()
+    const welcomeEmbed = new Fosscord.MessageEmbed()
 
     welcomeEmbed.setColor('#5cf000')
     welcomeEmbed.setTitle('**' + member.user.username + '** is now Among Us other **' + member.guild.memberCount + '** people')
     welcomeEmbed.setImage('https://gadajcord.us/attachments/1022185769067941944/1029314932061089990/welcome.png')
 
-    member.guild.channels.cache.find(i => i.name === 'greetings').send(welcomeEmbed)
+    member.guild.channels.cache.find(i => i.name === 'general').send(welcomeEmbed)
 })
 
 bot.on('guildMemberRemove', member => {
-    const goodbyeEmbed = new Discord.MessageEmbed()
+    const goodbyeEmbed = new Fosscord.MessageEmbed()
 
     goodbyeEmbed.setColor('#f00000')
     goodbyeEmbed.setTitle('**' + member.user.username + '** was not the impostor there are **' + member.guild.memberCount + '** left Among Us')
     goodbyeEmbed.setImage('https://gadajcord.us/attachments/1022185769067941944/1029314912012316845/goodbye.png')
 
-    member.guild.channels.cache.find(i => i.name === 'greetings').send(goodbyeEmbed)
+    member.guild.channels.cache.find(i => i.name === 'general').send(goodbyeEmbed)
 })
 //Welcome & goodbye messages end\\
